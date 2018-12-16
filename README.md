03c627a398f5ba767b5d979a892a9d374dc27fc6
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-12-16 08:49:40 +0100

    adding given files

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# Cambio frontend challenge
-Use FHIR to help a clinician calculate BMI
+# Test assignment: Help a clinician to calculate BMI
+
+When working with CDS, while the patient records contains a lot of patient information, we often need to ask a clinician for additional information about a patient. Your task will be to render questions to assess a patient's BMI, send responses to an endpoint and then render an assessment back to the clinician.
+
+You have been provided with a small server that will serve static files from _./static_ and also provides an HTTP endpoint _POST /api/execute_.
+
+The end point is used to get a JSON structure containing a _questionnaire_ definition, your job is to create and send a _questionnaireResponse_ structure with the users input, and then render the resulting _assessment_.
+
+In this assignment you will work with models defined in a standard called [FHIR](http://hl7.org/fhir/), it is an extensive standard for structuring clinical data but you will be provided links through out this assignment to only the relevant sections to reduce the scope.
+
+A [FHIR Questionnaire](https://www.hl7.org/fhir/questionnaire.html#resource) is a structure that describes which questions that should be asked to the clinician, the UI should then generate a [FHIR QuestionnaireResponse](https://www.hl7.org/fhir/questionnaireresponse.html#resource) and send that back to _POST /api/execute_. That call will then return a [FHIR Observation](https://www.hl7.org/fhir/observation.html#resource) that should be rendered to inform the user about the assessment.
+
+When reviewing your solution we will look at the structure and readability of your code.
+
+## Requirements
+
+- The information in the UI needs to stay relevant at all times.
+- Error handling; if a XHR fails or gives invalid structures the user needs to be informed.
+- Use CSS to style the elements in a user friendly way.
+- The code that renders the questionnaire _must_ be written in a way that could render any questionnaire structure with the item types _decimal_ and _group_.
+- The questionnaire should not have a “submit” button, but each time an input is changed a new call to _POST /api/execution_ must be made.
+
+If you get stuck on any of the requirements, send us a question via email.
+
+## Implementation notes
+
+- Write the code in ECMAScript 6 or higher, we encourage use of the newest JavaScript/ECMAScript language features.
+- The only browser needs compatibility with is Chromium/Chrome 71+.
+- The code should be written without any UI frameworks (e.g. no React, Angular, Polymer, jQuery).
+
+## Delivery
+
+The final project shall be accessible through a git repository for download. The deadline for delivering the assignment will be communicated elsewhere. A demonstration of the implemented solution will take place during the week after the delivery together with the whole team.
+Clear instructions on how to set up and run the service need to be provided.
+
+The code together with the demonstration will provide the interviewers with an understanding of the applicant's coding style and design choices.
+
+## Start the Server
+
+    $ npm install
+    $ node server.js
---
+++ server.js
@@ -0,0 +1,168 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {join} = require('path');
+const Ajv = require('ajv');
+
+const server = express();
+const port = process.env.PORT || 8080;
+
+server.use('/', express.static(join(__dirname, 'static')));
+server.use(bodyParser.json());
+
+server.post('/api/execute', (request, response) => {
+  const inputValidator = getInputValidator();
+  const valid = inputValidator(request.body);
+  if (!valid) {
+    response.status(400).json(inputValidator.errors);
+    return;
+  }
+
+  const responseBody = {};
+
+  responseBody.questionnaire = getQuestionnaire();
+
+  const questionnaireResponse = request.body.questionnaireResponse;
+  if (questionnaireResponse) {
+    responseBody.assessment = getObservationFromQuestionnaireResponse(questionnaireResponse);
+  }
+
+  response.json(responseBody);
+});
+
+server.listen(port, (listenError) => {
+  if (listenError) {
+    throw listenError;
+  } else {
+    console.log(`Server listening on http://localhost:${port}`);
+  }
+});
+
+function getQuestionnaire() {
+  return {
+    resourceType: 'Questionnaire',
+    id: 'bmi',
+    item: [
+      {
+        linkId: 'heightWeight',
+        type: 'group',
+        item: [
+          {
+            linkId: 'height',
+            type: 'decimal',
+            text: 'Height (cm)'
+          },
+          {
+            linkId: 'weight',
+            type: 'decimal',
+            text: 'Weight (kg)'
+          }
+        ]
+      }
+    ]
+  };
+}
+
+const classifications = {
+  'Very severely underweight': 0,
+  'Severely underweight': 16,
+  'Underweight': 18.5,
+  'Normal (healthy weight)': 25,
+  'Overweight': 30,
+  'Obese Class I (Moderately obese)': 35,
+  'Obese Class II (Severely obese)': 40,
+  'Obese Class III (Very severely obese)': 45,
+  'Obese Class IV (Morbidly Obese)': 50,
+  'Obese Class V (Super Obese)': 60,
+  'Obese Class VI (Hyper Obese)': Number.MAX_VALUE
+};
+function getClassification(bmi) {
+  let result;
+
+  for (const [label, labelMaximumBMI] of Object.entries(classifications)) {
+    result = label;
+
+    if (bmi <= labelMaximumBMI) {
+      break;
+    }
+  }
+
+  return result;
+}
+
+function getObservationFromQuestionnaireResponse(questionnaireResponse) {
+  const heightWeight = questionnaireResponse.item.find((item) => item.linkId === 'heightWeight');
+  const height = heightWeight.item.find((item) => item.linkId === 'height');
+  const weight = heightWeight.item.find((item) => item.linkId === 'weight');
+  const bmi = Math.round(weight.valueDecimal / Math.pow(height.valueDecimal / 100, 2) * 100) / 100;
+
+  return {
+    resourceType: 'Observation',
+    valueQuantity: {
+      value: bmi,
+      unit: 'kg/m2'
+    },
+    interpretation: {
+      text: getClassification(bmi)
+    }
+  };
+}
+
+function getInputValidator() {
+  const ajv = new Ajv({allErrors: true});
+  return ajv.compile({
+    $schema: 'http://json-schema.org/draft-07/schema#',
+    properties: {
+      questionnaireResponse: {
+        type: 'object',
+        required: ['id', 'resourceType', 'item'],
+        properties: {
+          id: {enum: ['bmi']},
+          resourceType: {enum: ['QuestionnaireResponse']},
+          item: {
+            type: 'array',
+            items: {
+              oneOf: [{
+                type: 'object',
+                required: ['linkId', 'item'],
+                properties: {
+                  linkId: {enum: ['heightWeight']},
+                  item: {
+                    type: 'array',
+                    items: {
+                      anyOf: [
+                        {
+                          type: 'object',
+                          required: ['linkId', 'valueDecimal'],
+                          properties: {
+                            linkId: {enum: ['height']},
+                            valueDecimal: {
+                              type: 'number',
+                              minimum: 50,
+                              maximum: 300
+                            }
+                          }
+                        },
+                        {
+                          type: 'object',
+                          required: ['linkId', 'valueDecimal'],
+                          properties: {
+                            linkId: {enum: ['weight']},
+                            valueDecimal: {
+                              type: 'number',
+                              minimum: 10,
+                              maximum: 300
+                            }
+                          }
+                        }
+                      ]
+                    }
+                  }
+                }
+              }]
+            }
+          }
+        }
+      }
+    }
+  });
+}
