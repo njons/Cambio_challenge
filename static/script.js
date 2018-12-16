f1c1780ef6957b90cf2b0ecb501f759dfb188984
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-12-16 09:57:05 +0100

    adds label and input code to createElement()

--- static/script.js
+++ static/script.js
@@ -15,6 +15,23 @@ window.addEventListener('load', (event) => {
 const createElement = (data, element) => {
   console.log('this is data in createElement()', data);
   console.log('this is element', element);
+  if (element === 'label') {
+    const label = document.createElement('label');
+    label.for = data.linkId;
+    label.textContent = data.text;
+    return label;
+  } else if (element === 'input') {
+    const input = document.createElement('input');
+    input.id = data.linkId;
+    console.log('this is the id:', data.linkId);
+    input.dataset.text = data.text;
+    input.placeholder = `add ${data.linkId} here`;
+    input.name = data.linkId;
+    input.value = '';
+    input.type = 'text';
+    input.autocomplete = 'off';
+    return input;
+  }
 };
 
 // render the questionnaire from the json
@@ -26,8 +43,8 @@ const renderQuestionnaire = (json) => {
     // create a label and input for each item
     const label = createElement(inputData, 'label');
     const input = createElement(inputData, 'input');
-    questionnaireForm.appendChild(label);
-    questionnaireForm.appendChild(input);
+    // questionnaireForm.appendChild(label);
+    // questionnaireForm.appendChild(input);
   });
 };
 
